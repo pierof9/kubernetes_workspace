608bc470207ffb29a924de5b9200192ad7ab860f
pierof <piero.farina092@gmail.com>
2024-03-13 22:26:15 +0100

    updating config.py

--- analytics/config.py
+++ analytics/config.py
@@ -4,14 +4,18 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = os.environ.get("DB_USERNAME")
+db_username = os.environ.get("DB_USERNAME", "postgres_piero")
 db_password = os.environ.get("DB_PASSWORD")
-db_host = os.environ.get("DB_HOST")
-db_port = os.environ.get("DB_PORT")
-db_name = os.environ.get("DB_NAME")
+db_host = os.environ.get("DB_HOST", "postgresql-service.default.svc.cluster.local")
+db_port = os.environ.get("DB_PORT", "5432")
+db_name = os.environ.get("DB_NAME", "mydatabase")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+conn_string = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+app.config["SQLALCHEMY_DATABASE_URI"] = conn_string
+
+print("This is the connection:\n")
+print(conn_string)
 
 db = SQLAlchemy(app)
 
